3cfc620c63ffe5a5a343a8b264b3c6f93aa0967b
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2024-03-09 15:52:46 +0100

    initial commit

---
+++ src/DotNetLib/Lib.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DotNetLib
+{
+    public static class Lib
+    {
+        private static int s_CallCount = 1;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LibArgs
+        {
+            public IntPtr Message;
+            public int Number;
+        }
+
+        public static int Hello(IntPtr arg, int argLength)
+        {
+            if (argLength < System.Runtime.InteropServices.Marshal.SizeOf(typeof(LibArgs)))
+            {
+                return 1;
+            }
+
+            LibArgs libArgs = Marshal.PtrToStructure<LibArgs>(arg);
+            Console.WriteLine($"Hello, world! from {nameof(Lib)} [count: {s_CallCount++}]");
+            PrintLibArgs(libArgs);
+            return 0;
+        }
+
+        [UnmanagedCallersOnly]
+        public static unsafe void TestFnPtr(delegate*<void> fn_from_cpp)
+        {
+            Console.WriteLine($"[C#] Preparing to call c++ function");
+            fn_from_cpp();
+        }
+        
+        [UnmanagedCallersOnly]
+        public static unsafe void TestFnPtrWithArgs(delegate* unmanaged[MemberFunction]<int, double> fn_from_cpp)
+        {
+            Console.WriteLine($"[C#] Preparing to call c++ function");
+            double ret = fn_from_cpp(20);
+            Console.WriteLine($"[C#] C++ function returned {ret}!");
+        }
+        
+        public delegate void CustomEntryPointDelegate(LibArgs libArgs);
+        public static void CustomEntryPoint(LibArgs libArgs)
+        {
+            Console.WriteLine($"Hello, world! from {nameof(CustomEntryPoint)} in {nameof(Lib)}");
+            PrintLibArgs(libArgs);
+        }
+
+        [UnmanagedCallersOnly]
+        public static void CustomEntryPointUnmanagedCallersOnly(LibArgs libArgs)
+        {
+            Console.WriteLine($"Hello, world! from {nameof(CustomEntryPointUnmanagedCallersOnly)} in {nameof(Lib)}");
+            PrintLibArgs(libArgs);
+        }
+
+        private static void PrintLibArgs(LibArgs libArgs)
+        {
+            string message = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? Marshal.PtrToStringUni(libArgs.Message)
+                : Marshal.PtrToStringUTF8(libArgs.Message);
+
+            Console.WriteLine($"-- message: {message}");
+            Console.WriteLine($"-- number: {libArgs.Number}");
+        }
+    }
+}
